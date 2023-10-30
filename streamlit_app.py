6ef1574b142a582a1d9f611bbd7bd57c1e8e0474
Jolene RP <jolene_chiam@rp.edu.sg>
2023-10-30 09:43:22 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,67 +5,110 @@ from langchain.text_splitter import CharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chains import RetrievalQA
-import textract
+from PyPDF2 import PdfReader
+from io import BytesIO
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from pptx import Presentation
+from docx import Document
+from langchain.text_splitter import TokenTextSplitter
 
 # Install sqlite3 module
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-# Define a function to generate response from uploaded files and query text
-def generate_response(uploaded_files, openai_api_key, query_text):
-    # Initialize an empty list to store documents
-    documents = []
-    # Loop through the uploaded files
-    for uploaded_file in uploaded_files:
-        # Parse the document based on its type
-        if uploaded_file.type == 'application/pdf':
-            # Get the file content as bytes and pass it to textract.process with extension argument
-            document = textract.process(uploaded_file.getvalue(), extension='pdf', method='pdfminer').decode()
-        elif uploaded_file.type == 'text/plain':
-            # Read the file content as string
-            document = uploaded_file.read().decode()
-        elif uploaded_file.type == 'application/vnd.openxmlformats-officedocument.wordprocessingml.document':
-            # Get the file content as bytes and pass it to textract.process with extension argument
-            document = textract.process(uploaded_file.getvalue(), extension='docx').decode()
-        else:
-            # Skip unsupported file types
-            continue
-        # Append the document to the list of documents
-        documents.append(document)
-    # Split documents into chunks using CharacterTextSplitter
-    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
-    texts = text_splitter.create_documents(documents)
-    # Select embeddings using OpenAIEmbeddings
-    embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
-    # Create a vectorstore from documents using Chroma
-    db = Chroma.from_documents(texts, embeddings)
-    # Create retriever interface from vectorstore
-    retriever = db.as_retriever()
-    # Create QA chain using RetrievalQA, OpenAI, and retriever
-    qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
-    # Run the QA chain with the query text and return the response
-    return qa.run(query_text)
+def read_docx(file_path):
+    document = Document(file_path)
+    text = ''
+    for paragraph in document.paragraphs:
+        text += paragraph.text
+    return text
+
+def read_pptx(file_path):
+    presentation = Presentation(file_path)
+    text = ''
+    for slide in presentation.slides:
+        for shape in slide.shapes:
+            if shape.has_text_frame:
+                for paragraph in shape.text_frame.paragraphs:
+                    for run in paragraph.runs:
+                        text += run.text
+    return text
+
+def generate_response(uploaded_file, openai_api_key, query_text):
+    # Load document if file is uploaded
+    if uploaded_file is not None:
+        # Get the file extension
+        file_type = uploaded_file.type
+
+        # Use PyPDF2 to read the PDF file
+        if file_type == "application/pdf":
+            pdf = PdfReader(BytesIO(uploaded_file.getvalue()))
+            # Extract text from each page and add it to the text string
+            text = ''
+            for page in pdf.pages:
+                text += page.extract_text()
+                
+        # Read the TXT file
+        elif file_type == "text/plain":
+            text = uploaded_file.getvalue().decode()
+            
+        elif "presentationml" in file_type: # pptx file
+            text = read_pptx(uploaded_file)
+            
+        elif "wordprocessingml" in file_type: # docx file
+            text = read_docx(uploaded_file)
+
+        documents = [text]
+        # Split documents into chunks
+        text_splitter = TokenTextSplitter(
+            chunk_size=800, 
+            chunk_overlap=10
+            )
+        # text_splitter = RecursiveCharacterTextSplitter(
+        #     # In English, one token is roughly equivalent to 4 characters1. 
+        #     # So, approximately 3500 tokens would be around 14,000 characters.
+        #     chunk_size=3500, 
+        #     chunk_overlap=20,
+        #     length_function=len,
+        #     is_separator_regex=False,
+        # )
+        texts = text_splitter.create_documents(documents)
+
+        # Select embeddings
+        embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
+        # Create a vectorstore from documents
+        # Chroma vectorstoreï¼šdesigned for short documents, such as tweets and product descriptions. It is not ideal for long documents, such as PDF files and presentations.
+        # HNSW vectorstore: This vectorstore is designed for long documents, and it is more efficient than the Chroma vectorstore.
+        db = Chroma.from_documents(texts, embeddings)
+        
+        # Create retriever interface
+        retriever = db.as_retriever()
+        
+        # Create QA chain
+        qa = RetrievalQA.from_chain_type(llm=OpenAI(openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
+        return qa.run(query_text)
 
 # Page title
-st.set_page_config(page_title='🦜🔗 Ask the Doc App')
-st.title('🦜🔗 Ask the Doc App')
+st.set_page_config(page_title='ðŸ¦œðŸ”— Ask the Doc App')
+st.title('ðŸ¦œðŸ”— Ask the Doc App')
 
-# File upload (allow multiple files)
-uploaded_files = st.file_uploader('Upload one or more articles', type=['pdf', 'txt', 'docx'], accept_multiple_files=True)
+# File upload
+uploaded_file = st.file_uploader('Upload a PDF, PPTX, DOCX or TXT file', type=["pdf", "txt", "pptx", "docx"])
 # Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_files)
+query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_file)
 
+openai_api_key = "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"
 # Form input and query
 result = []
 with st.form('myform', clear_on_submit=True):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_files and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_files and query_text))
+    # openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_file and query_text))
+    submitted = st.form_submit_button('Submit', disabled=not(uploaded_file and query_text))
     if submitted and openai_api_key.startswith('sk-'):
         with st.spinner('Calculating...'):
-            response = generate_response(uploaded_files, openai_api_key, query_text)
+            response = generate_response(uploaded_file, openai_api_key, query_text)
             result.append(response)
-            del openai_api_key
+            # del openai_api_key
 
 if len(result):
     st.info(response)
